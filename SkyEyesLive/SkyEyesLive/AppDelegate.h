67184bb41f061e44116cc1b7f086068da656a1c4
sunda <sunda@51fa3a50-ca6c-4946-aef2-29cb9e7371e5>
2015-08-24 07:23:30 +0000

    ios 项目初始化

---
+++ SkyEyesLive/SkyEyesLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SkyEyesLive
+//
+//  Created by sunda on 15/8/24.
+//  Copyright (c) 2015年 sunda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
